6df3678f533f532be400b59eceb6f3e7e0d996c4
sirSayed98 <mohamed.sayed8899@gmail.com>
2024-11-21 19:02:15 +0200

    feat: :fire: add splash component

---
+++ src/components/Splash.tsx
@@ -0,0 +1,42 @@
+
+import { Box, Typography } from "@mui/material";
+import LinearProgress from '@mui/material/LinearProgress';
+
+const SplashScreen = () => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#f4f4f4",
+        height: "100vh",
+      }}
+    >
+      <Box>
+        <img
+          src='./nasdaq.png'
+          alt="Nasdaq Logo"
+          style={{
+            width: "150px",
+            marginBottom: "auto",
+          }}
+        />
+        <LinearProgress />
+      </Box>
+
+      <Typography
+        sx={{
+          position: "absolute",
+          bottom: 16,
+          color: "#666",
+        }}
+      >
+        Developed by Mohamed Sayed
+      </Typography>
+    </Box>
+  );
+};
+
+export default SplashScreen
