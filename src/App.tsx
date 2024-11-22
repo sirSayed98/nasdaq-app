30d6779a68315e21e4e2b9a7c23179029f81ca03
sirSayed98 <mohamed.sayed8899@gmail.com>
2024-11-22 13:15:04 +0200

    feat: :fire: handle App routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
-import Home from './pages/Home'
+// react -libs
+import { Suspense, lazy } from 'react';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+
+// pages
+import NotFoundPage from '@pages/Not-Found';
+import SplashScreen from '@pages/Splash';
+// Lazy load the Home component
+const Home = lazy(() => import('@pages/Home'));
+
 const App = () => {
   return (
-    <><Home /></>
+    <>
+      <Router>
+        <Suspense fallback={<SplashScreen />}>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='*' element={<NotFoundPage />} />
+          </Routes>
+        </Suspense>
+      </Router>
+    </>
   )
 }
 
